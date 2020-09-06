8fc3d8bc0982c00bcbd4c58b724a5548cce075b0
DCATANOR\dcatanor <dasniel199410@gmail.com>
2020-09-06 16:42:20 -0500

    Creating user schema

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const { MONGO_URI } = require('./keys');
 const app = express();
 const PORT = 5000;
 
+require('./model/user');
+app.use(require('./routes/auth'));
+
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.on('connected', () => console.log('Mongo Db connection success'));
 mongoose.connection.on('error', e => console.error('Error connecting', e));
