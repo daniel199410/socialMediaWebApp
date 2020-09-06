ebde3859f096700ba8a1bdbed8e3664c1b038c76
DCATANOR\dcatanor <dasniel199410@gmail.com>
2020-09-06 16:28:10 -0500

    Initial setup and initial Mongo config

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { MONGO_URI } = require('./keys');
+const app = express();
+const PORT = 5000;
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.on('connected', () => console.log('Mongo Db connection success'));
+mongoose.connection.on('error', e => console.error('Error connecting', e));
+
+app.listen(PORT, () => console.log(`Server is running at port ${PORT}`));
---
+++ keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGO_URI: 'mongodb+srv://dcatanor:LUAx5chibFr8qiOT@cluster0.8gmnp.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
