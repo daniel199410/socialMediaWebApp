48b24c1ca62dffa9cf7ee4190fdf6838e900dc5e
DCATANOR\dcatanor <dasniel199410@gmail.com>
2020-09-06 17:43:03 -0500

    Returning jwt token on successful sign in

--- keys.js
+++ keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    MONGO_URI: 'mongodb+srv://dcatanor:LUAx5chibFr8qiOT@cluster0.8gmnp.mongodb.net/socialMedia?retryWrites=true&w=majority'
+    MONGO_URI: 'mongodb+srv://dcatanor:LUAx5chibFr8qiOT@cluster0.8gmnp.mongodb.net/socialMedia?retryWrites=true&w=majority',
+    JWT_SECRET: 'koidum=?837638dkdkeo'
 }
