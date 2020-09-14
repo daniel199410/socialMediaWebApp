79bebc2a88133bda85aaa264ede4a4235c8c89b2
--get <--get>
2020-09-14 16:27:49 -0500

    Saving posts

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const { MONGO_URI } = require('./keys');
 const app = express();
 const PORT = 5000;
 
-app.use(express.json());
-require('./model/user');
-app.use(require('./routes/auth'));
-
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.on('connected', () => console.log('Mongo Db connection success'));
 mongoose.connection.on('error', e => console.error('Error connecting', e));
+
+app.use(express.json());
+require('./model/user');
+require('./model/post');
+
+app.use(require('./routes/auth'));
+app.use(require('./routes/post'));
 
 app.listen(PORT, () => console.log(`Server is running at port ${PORT}`));
--- routes/auth.js
+++ routes/auth.js
@@ -5,11 +5,6 @@ const User = mongoose.model('User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../keys');
-const requireLogin = require('../middleware/requireLogin');
-
-router.get('/protected', requireLogin, (req, res) => {
-    res.send('Hello');
-})
 
 router.post('/signup', (req, res) => {
     const { name, email, password} = req.body;
